5e174417c8ff26c91696a82d65377251c45211d2
Eric <meisericxie@gmail.com>
2023-01-10 19:09:06 -0800

    another thing

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,6 +1,7 @@
 const Images = {
     "Portifolio Website": {"path": require("../assests/React_MUI.png"), "data": "logo"},
     "Full Stack Email Website": {"path": "", "data": "false"},
+    "HTTP server": {"path": require("../assests/c.png"), "data": "logo"}
 }
 
 export default Images;
